098a8ac14e0fe41fe7d5ef1625aaf010ab3ecee7
Nathan Levesque <nlevesq@mit.edu>
2024-07-17 14:37:59 -0400

    Fix docker-compose, add a nix setup, other fixes + cleanup

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "ol-django development flake";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          packages = [
+            pkgs.rye
+          ];
+        };
+      };
+    };
+}
